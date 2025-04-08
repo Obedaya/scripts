3a1c1da46565764d20b86d1caf2244cd36c0e889
Obedaya <yeneuro.mail@gmail.com>
2025-04-08 15:08:30 +0200

    Added optional setup

--- Setup.sh
+++ Setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+INSTALL_ZSH=false
+
 echo "Get required files..."
-git clone https://github.com/Obedaya/obedssetupscript
-cd obedssetupscript
-chmod +x setup.sh
-./setup.sh -t
+while getopts "t" opt; do
+    case $opt in
+      t) INSTALL_ZSH=true ;;
+      \?) echo "Invalid option -$OPTARG" >&2; exit 1 ;;
+    esac
+done
+
+if $INSTALL_TERMINATOR; then
+	git clone https://github.com/Obedaya/obedssetupscript
+	cd obedssetupscript
+	chmod +x setup.sh
+	./setup.sh -t
+fi
 
 echo "Unpacking rockyou..."
 sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
